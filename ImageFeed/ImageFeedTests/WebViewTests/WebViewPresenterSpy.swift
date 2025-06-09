a728e408a77ae5da563aa49381020cda0f9803c1
timg777 <timg777@yandex.ru>
2025-06-09 18:16:00 +0500

    test(SplashViewTests): Добавление тестов
    Написаны Unit тесты, проверяющие логику работы презентера
    Добавлены сущности-шпионы, имитирующие своих оригиналов, собирая доп инфу
    
    META: sprint_13,  ImageFeed

---
+++ ImageFeed/ImageFeedTests/WebViewTests/WebViewPresenterSpy.swift
@@ -0,0 +1,34 @@
+@testable import ImageFeed
+import WebKit
+
+final class WebViewPresenterSpy: WebViewPresenterProtocol {
+    
+    var authHelper: AuthHelperProtocol?
+    private(set) var viewDidiLoadCalled = false
+    var view: WebViewControllerProtocol?
+    
+    init(authHelper: AuthHelperProtocol? = nil) {
+        self.authHelper = authHelper
+    }
+    
+    func viewDidLoad() {
+        viewDidiLoadCalled = true
+        guard let request = authHelper?.authRequest() else {
+            logErrorToSTDIO(
+                errorDescription: "Failed to create URLRequest by OAouth2Service.getUserAuthRequest"
+            )
+            return
+        }
+        view?.load(request: request)
+    }
+    
+    func didUpdateProgressValue(_ progress: Float) {}
+    
+    func shouldHideProgress(for progress: Float) -> Bool {
+        true
+    }
+    
+    func code(from navigationAction: WKNavigationAction) -> String? {
+        nil
+    }
+}
